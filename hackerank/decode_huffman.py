2c5e292b71e63f0314ffc4352f5b13d16f59742d
Levy-Naibei <levynaibei2@gmail.com>
2023-12-21 22:14:49 +0300

    solved huffman decoding

---
+++ hackerank/decode_huffman.py
@@ -0,0 +1,72 @@
+import queue as Queue
+
+cntr = 0
+
+class Node:
+    def __init__(self, freq, data):
+        self.freq = freq
+        self.data = data
+        self.left = None
+        self.right = None
+        global cntr
+        self._count = cntr
+        cntr = cntr + 1
+        
+    def __lt__(self, other):
+        if self.freq != other.freq:
+            return self.freq < other.freq
+        return self._count < other._count
+
+def huffman_hidden():#builds the tree and returns root
+    q = Queue.PriorityQueue()
+
+    
+    for key in freq:
+        q.put((freq[key], key, Node(freq[key], key) ))
+    
+    while q.qsize() != 1:
+        a = q.get()
+        b = q.get()
+        obj = Node(a[0] + b[0], '\0' )
+        obj.left = a[2]
+        obj.right = b[2]
+        q.put((obj.freq, obj.data, obj ))
+        
+    root = q.get()
+    root = root[2]#contains root object
+    return root
+
+def dfs_hidden(obj, already):
+    if(obj == None):
+        return
+    elif(obj.data != '\0'):
+        code_hidden[obj.data] = already
+        
+    dfs_hidden(obj.right, already + "1")
+    dfs_hidden(obj.left, already + "0")
+
+"""class Node:
+    def __init__(self, freq,data):
+        self.freq= freq
+        self.data=data
+        self.left = None
+        self.right = None
+"""        
+
+def decode_huff(root, s):
+    cur_node = root
+    decoded_str = ""
+    index = 0
+    while index < len(s):
+        if s[index] == "0":
+            cur_node = cur_node.left
+        elif s[index] == "1":
+            cur_node = cur_node.right
+        
+        if cur_node.left is None and cur_node.right is None:
+            decoded_str += cur_node.data
+            cur_node = root
+        
+        index += 1
+    print(decoded_str)
+print(decode_huff())
