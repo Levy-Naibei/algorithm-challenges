392547dc94b323d0860b4f2312c6494a24340815
Levy-Naibei <levynaibei2@gmail.com>
2023-12-27 23:22:59 +0300

    solved time conversion

--- hackerank/candies.py
+++ hackerank/candies.py
@@ -1,4 +1,9 @@
 """
+Alice is a kindergarten teacher. She wants to give some candies to the children in her class. 
+All the children sit in a line and each of them has a rating score according to his or her performance in the class. 
+Alice wants to give at least 1 candy to each child. If two children sit next to each other, 
+then the one with the higher rating must get more candies.
+Alice wants to minimize the total number of candies she must buy.
 """
 def candies(n, arr):
     candy = [1] * n
@@ -11,4 +16,6 @@ def candies(n, arr):
         
     return sum(candy)
 
-# print(candies())
+print(candies(3, [1, 2, 2]))
+print(candies(10, [2, 4, 2, 6, 1, 7, 8, 9, 2, 1]))
+print(candies(8, [2, 4, 3, 5, 2, 6, 4, 5]))
